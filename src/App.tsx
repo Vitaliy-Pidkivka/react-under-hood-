61221de54b29570aad9244395bde4752d26db563
DESKTOP-99OH5VO\Vitalii <pidkivka_vv@groupbwt.com>
2021-06-21 16:32:48 +0300

    add App.tsx
    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './App.css'
 
 const App: React.FC = () => {
-
-  return (<div>Base</div>)
+  return (
+    <div>
+      <div>
+        <div></div>
+        sad <div>sds</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
